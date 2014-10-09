35ee87fa3464cddcff790aca90863139aded2764
ruygarcia <rrgarciach@gmail.com>
2014-10-09 00:52:03 -0500

    Performing first communication test with Bluetooth.

--- RFID_Access_Servo__RC522/RFID_Access_Servo__RC522.ino
+++ RFID_Access_Servo__RC522/RFID_Access_Servo__RC522.ino
@@ -5,11 +5,14 @@
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);	// Create MFRC522 instance.
-SoftwareSerial bt(4, 5); // RX, TX
+SoftwareSerial bt(5, 4); // RX, TX
 int outputPin = 8;
+String btStringBuffer;
 
 void setup() {
   Serial.begin(115200);	// Initialize serial communications with the PC
+  bt.begin(9600);
+  //btSetup();
   SPI.begin();		// Init SPI bus
   mfrc522.PCD_Init();	// Init MFRC522 card
   Serial.println("RC522 ready.");
@@ -27,6 +30,12 @@ void loop() {
     return;
   }
   checkAccess();
+  if( bt.available() ) {
+    while( bt.available() ) {
+      btStringBuffer += (char)bt.read();
+    }
+  }
+  Serial.println(btStringBuffer);
 }
 
 void checkAccess() {  
@@ -38,17 +47,45 @@ void checkAccess() {
     cardID += String(mfrc522.uid.uidByte[i], HEX);
   }
   Serial.println(cardID);
+
   
-  Serial.println();
+  if( bt.available() ) {
+    while( bt.available() ) {
+      btStringBuffer += (char)bt.read();
+    }
+  }
+    
   if (cardID == "bafc6fd5") {
     Serial.println("Access!!!");
+    bt.println("Access!!!");
     
     //myservo.write(180);
     beep(true);
     delay(2000);
     //myservo.write(0);
   }
+  Serial.println("Access NOT ALLOWED.");
+  bt.println("Access NOT ALLOWED.");
   beep(false);
+}
+
+void btSetup() {
+  // Starts AT command state
+  bt.println("AT");
+  // Perform synchronous communication
+  while( bt.available() ) {
+    Serial.println(bt.read());
+  }
+  // Sets device name
+  bt.println("AT+NAME=DOOR-729");
+  while( bt.available() ) {
+    Serial.println(bt.read());
+  }
+  // Sets device PIN
+  bt.println("AT+PSWD=2274");
+  while( bt.available() ) {
+    Serial.println(bt.read());
+  }
 }
 
 void beep(boolean access) {
